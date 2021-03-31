b2f27055b23dcaeb5c388277df8022faaa975c52
Milad <milad.roygari@gmail.com>
2021-03-31 13:05:44 -0700

    removed extra

--- contentscript.js
+++ contentscript.js
@@ -1,38 +1,34 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.type === "start") {
-        start();
-    }
-    sendResponse({});
+chrome.extension.onMessage.addListener(function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.type === "start") {
+    start();
+  }
+  sendResponse({});
 });
 
 function send(request) {
-    chrome.extension.sendMessage(request, function(response) {});
+  chrome.extension.sendMessage(request, function (response) {});
 }
 
-var BORDER_THICKNESS = 4;
-var overlay, outline;
-
 function start() {
+  var element,
+    dimensions = {};
 
-    var element, dimensions = {};
-    element = document.getElementsByClassName("panel panel-default")[2];
-    dimensions.top = -window.scrollY;
-    dimensions.left = -window.scrollX;
-    var elem = document.getElementsByClassName("panel panel-default")[2];
-    while (elem !== document.body) {
-        dimensions.top += elem.offsetTop;
-        dimensions.left += elem.offsetLeft;
-        elem = elem.offsetParent;
-    }
-    dimensions.width = element.offsetWidth;
-    dimensions.height = element.offsetHeight
-
-    outline.style.top = (dimensions.top - BORDER_THICKNESS) + "px";
-    outline.style.left = (dimensions.left - BORDER_THICKNESS) + "px";
-    outline.style.width = dimensions.width + "px";
-    outline.style.height = dimensions.height + "px";
-
-    send({ type: "up", dimensions: dimensions });
+  element = document.getElementsByClassName("panel panel-default")[2];
+  dimensions.top = -window.scrollY;
+  dimensions.left = -window.scrollX;
+  var elem = document.getElementsByClassName("panel panel-default")[2];
+  while (elem !== document.body) {
+    dimensions.top += elem.offsetTop;
+    dimensions.left += elem.offsetLeft;
+    elem = elem.offsetParent;
+  }
+  dimensions.width = element.offsetWidth;
+  dimensions.height = element.offsetHeight;
+  send({ type: "up", dimensions: dimensions });
 }
 
 send({ type: "enable" });
