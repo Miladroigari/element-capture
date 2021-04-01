a9de7acecfa6203d795e5309ac9a5d86b30988e6
Milad <milad.roygari@gmail.com>
2021-03-31 17:03:53 -0700

    added clipboard

--- contentscript.js
+++ contentscript.js
@@ -18,17 +18,31 @@ function start() {
     dimensions = {};
 
   element = document.getElementsByClassName("panel panel-default")[2];
-  dimensions.top = -window.scrollY;
-  dimensions.left = -window.scrollX;
-  var elem = document.getElementsByClassName("panel panel-default")[2];
-  while (elem !== document.body) {
-    dimensions.top += elem.offsetTop;
-    dimensions.left += elem.offsetLeft;
-    elem = elem.offsetParent;
+  products = element.getElementsByClassName("ui-grid-canvas")[0].children;
+  var output = "Invoice:\n";
+  var total = 0;
+  //output += 'Product \t price \t size \t subtotal\n';
+  for (item of products) {
+    var text = item.innerText;
+    const [title, brand, price, size, subtotal] = text.split("\n");
+    total += parseFloat(subtotal.replace("$", ""));
+    output += `${brand}-${title}\t${price}\t${size}\t${subtotal}\n`;
   }
-  dimensions.width = element.offsetWidth;
-  dimensions.height = element.offsetHeight;
-  send({ type: "up", dimensions: dimensions });
+  var tax = total * 0.1;
+  output += `Total = $${total}\n`;
+  output += `Tax(10%) = ${tax}\n`;
+  total = total + tax;
+  output += `Subtotal=${total}`;
+  console.log(output);
+
+  navigator.clipboard.writeText(output).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
 }
 
 send({ type: "enable" });
